02ebe49aa0b7f37865efba4b543d138c9dd2afd8
JEVietti <dovajoe@gmail.com>
2016-02-05 21:16:52 -0800

    Add Some user interface and Activities/Functionality

---
+++ app/src/main/java/roast/app/com/dealbreaker/InitialScreen.java
@@ -0,0 +1,43 @@
+package roast.app.com.dealbreaker;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class InitialScreen extends AppCompatActivity {
+    private Button registerButton,loginButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initial_screen);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        registerButton = (Button) findViewById(R.id.register_button);
+        loginButton = (Button) findViewById(R.id.login_button);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(InitialScreen.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(InitialScreen.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/roast/app/com/dealbreaker/RegisterActivity.java
@@ -0,0 +1,20 @@
+package roast.app.com.dealbreaker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.parse.ParseFacebookUtils;
+import com.parse.ParseUser;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+    }
+    public void createUser(){
+
+    }
+}
