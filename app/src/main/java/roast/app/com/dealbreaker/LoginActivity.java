3a8ba72e69c9a34db255791821e658631f0b32b7
JEVietti <dovajoe@gmail.com>
2016-03-08 17:59:25 -0800

    Fixing some of the files from the last commit

--- app/src/main/java/roast/app/com/dealbreaker/LoginActivity.java
+++ app/src/main/java/roast/app/com/dealbreaker/LoginActivity.java
@@ -3,30 +3,25 @@ package roast.app.com.dealbreaker;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.Button;
+import android.widget.Toast;
+
+import com.firebase.client.Firebase;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import roast.app.com.dealbreaker.util.Constants;
 
 public class LoginActivity extends AppCompatActivity {
-    private Button profileButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        Intent intent = getIntent();
+        String userEmail = intent.getStringExtra(getString(R.string.key_UserName));
 
-        profileButton = (Button) findViewById(R.id.button);
-
-        profileButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, ProfileActivity.class);
-                startActivity(intent);
-            }
-        });
-
-
+        Toast.makeText(this, "Login successful for " + userEmail, Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/roast/app/com/dealbreaker/UserNavigation.java
+++ app/src/main/java/roast/app/com/dealbreaker/UserNavigation.java
@@ -18,33 +18,41 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.AdapterView;
+import android.widget.Toast;
 
 import roast.app.com.dealbreaker.fragments.UserAttribute;
 
+/* The UserNavigation class is meant as the base means of navigation during the app states
+* of which include the User's Home page, Images, Preferences and User Information Tasks (Fragments)
+*
+* */
 public class UserNavigation extends AppCompatActivity{
-    private String userName = "password";
+    private String userName;
     private DrawerLayout mDrawer;
     private NavigationView nvDrawer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_navigation);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, mDrawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        mDrawer.setDrawerListener(toggle);
-        toggle.syncState();
-        nvDrawer = (NavigationView) findViewById(R.id.nav_view);
-        setupDrawerContent(nvDrawer);
-
-        if (savedInstanceState == null) {
-            setFirstItemNavigationView();
+        //Get UserName data passed from the Login
+        if(getIntent().getExtras() != null) {
+            Bundle arg = getIntent().getExtras();
+            userName = arg.getString("username");
+            //Initialize the View of the Fragment
+            initializeView();
+            //Set the first Fragment to be loaded
+            if (savedInstanceState == null) {
+                setFirstItemNavigationView();
+            }
+        }
+        else {
+            finish();
+            Toast.makeText(getApplicationContext(), "Failed to retrieve User's Data", Toast.LENGTH_SHORT).show();
         }
     }
 
+    //Changes the default onBackPressed so that the User can get a warning to whether they would like to exit or not
     @Override
     public void onBackPressed() {
         if (mDrawer.isDrawerOpen(GravityCompat.START)) {
@@ -53,12 +61,12 @@ public class UserNavigation extends AppCompatActivity{
             AlertDialog.Builder dlg = new AlertDialog.Builder(this,R.style.AlertDialogTheme);
             dlg.setTitle("Really Exit!");
             dlg.setMessage("Are you Sure you want to Exit?");
-                    dlg.setNegativeButton(android.R.string.no, null);
-                    dlg.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface arg0, int arg1) {
-                            UserNavigation.super.onBackPressed();
-                        }
-                    }).create().show();
+            dlg.setNegativeButton(android.R.string.no, null);
+            dlg.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface arg0, int arg1) {
+                    UserNavigation.super.onBackPressed();
+                }
+            }).create().show();
         }
     }
 
@@ -76,10 +84,11 @@ public class UserNavigation extends AppCompatActivity{
                 mDrawer.openDrawer(GravityCompat.START);
                 return true;
         }
-
-
         return super.onOptionsItemSelected(item);
     }
+    //Sets the DrawerContent to be able to be changed based on Different Navigation Drawer Items
+    //using the helper functions NavItemSelected which actually calls the other fragments based on
+    //the menu item selected.
     private void setupDrawerContent(NavigationView navigationView) {
         navigationView.setNavigationItemSelectedListener(
                 new NavigationView.OnNavigationItemSelectedListener() {
@@ -91,21 +100,23 @@ public class UserNavigation extends AppCompatActivity{
                 });
     }
 
-
+    //
     @SuppressWarnings("StatementWithEmptyBody")
     private boolean NavItemSelected(MenuItem item) {
         // Create a new fragment and specify the planet to show based on
         // position
         Fragment fragment = null;
+        //Switches to different fragments based on the id in the Navigation Drawer Mennu
+        //which can be found in the DealBreaker\app\src\main\res\menu\activity_user_navigation_drawer.xml file
         switch (item.getItemId()) {
             case R.id.nav_manage:
-                fragment = AttributeAssignment.newInstance("password");
+                fragment = AttributeAssignment.newInstance(userName);
                 break;
             case R.id.nav_send:
-                fragment = UserAttribute.newInstance("password");
+                fragment = ProfileActivity.newInstance(userName);
                 break;
             default:
-                fragment = AttributeAssignment.newInstance("password");
+                fragment = ProfileActivity.newInstance(userName);
                 break;
         }
 
@@ -121,41 +132,26 @@ public class UserNavigation extends AppCompatActivity{
         return true;
     }
 
-
-    private void setFirstItemNavigationView() {
-         nvDrawer.setCheckedItem(R.id.nav_manage);
-         nvDrawer.getMenu().performIdentifierAction(R.id.nav_manage, 0);
-         nvDrawer.setCheckedItem(R.id.nav_manage);
-}
-/*
-    private void selectDrawerItem(MenuItem menuItem) {
-        // Create a new fragment and specify the planet to show based on
-        // position
-        Fragment fragment = null;
-        switch (menuItem.getItemId()) {
-            case R.id.nav_manage:
-                fragment = AttributeAssignment.newInstance("password");
-                break;
-            case R.id.nav_send
-                fragment = UserAttribute.newInstance("password");
-                break;
-            default:
-                fragment = AttributeAssignment.newInstance("password");
-                break;
-        }
-
-        // Insert the fragment by replacing any existing fragment
-        if(fragment!=null) {
-            FragmentManager fragmentManager = getSupportFragmentManager();
-            fragmentManager.beginTransaction().replace(R.id.Content, fragment).commit();
-        }
-        // Highlight the selected item, update the title, and close the drawer
-        menuItem.setChecked(true);
-        setTitle(menuItem.getTitle());
-        mDrawer.closeDrawers();
-
+    //Initializes the view for this Activity
+    private void initializeView(){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //Sets up the Drawer
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, mDrawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawer.setDrawerListener(toggle);
+        toggle.syncState();
+        nvDrawer = (NavigationView) findViewById(R.id.nav_view);
+        setupDrawerContent(nvDrawer);
     }
-*/
 
+    //Sets the Fragment that will be loaded up implicitly
+    //by calling the setCheckedItem Helper function and using this function in onCreate
+    private void setFirstItemNavigationView() {
+        nvDrawer.setCheckedItem(R.id.nav_send);
+        nvDrawer.getMenu().performIdentifierAction(R.id.nav_send, 0);
+        nvDrawer.setCheckedItem(R.id.nav_send);
+    }
 
 }
