90ebf2b4b078e14cae87c309d8a7532a149de71a
Jschott9 <Jarrett@MacBook-Pro.local>
2016-04-30 12:28:40 -0700

    Roaming Profile and Activity

---
+++ app/src/main/java/roast/app/com/dealbreaker/RoamingProfile.java
@@ -0,0 +1,239 @@
+package roast.app.com.dealbreaker;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import roast.app.com.dealbreaker.fragments.UpdateImage;
+import roast.app.com.dealbreaker.models.User;
+import roast.app.com.dealbreaker.models.UserQualities;
+import roast.app.com.dealbreaker.util.Constants;
+import roast.app.com.dealbreaker.util.DownloadImages;
+
+public class RoamingProfile extends Fragment {
+
+    private TextView bio_info,goodQualitiesInfo,badQualitiesInfo, personalName,age, location, sexText;
+    private ImageButton imageButton;
+    private String userName;
+    private String key, profilePicURL;
+    private DownloadImages downloadImages;
+    private Firebase userInfoREF, userQualitiesREF, profilePicREF;
+    private ValueEventListener userInfoListener, userQualitiesListener, profilePicListener;
+
+
+    private GoogleApiClient client;
+    public static RoamingProfile newInstance(String userName) {
+        RoamingProfile fragment = new RoamingProfile();
+        Bundle args = new Bundle();
+        //Adding the userName to the Bundle in order to use it later on
+        args.putString("userName",userName);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_profile);
+        if (getArguments() != null) {
+            key = getString(R.string.key_UserName);
+            userName = getArguments().getString(key);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             final Bundle savedInstanceState) {
+        // Initialize UI elements
+        final View view = inflater.inflate(R.layout.activity_profile, container, false);
+        initializeScreen(view);
+        //setSupportActionBar(toolbar);
+        //super.getView().setLabelFor(R.id.toolbar);
+        //Set an on click listener that switches to another activity or fragment in which a user can
+        //change their bio, good and bad qualities as well as select to upload a different image
+        // as their profile picture
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment f = null;
+                f = UpdateImage.newInstance(userName);
+                if(f != null && savedInstanceState == null) {
+                    FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.Content, f);
+                    fragmentTransaction.addToBackStack(null);
+                    fragmentTransaction.commit();
+                }
+            }
+        });
+        return view;
+    }
+
+    private void initializeScreen(View rootView) {
+        //profile_info = (TextView) rootView.findViewById(R.id.textView);
+        personalName = (TextView) rootView.findViewById(R.id.roamingNameTextView);
+        bio_info = (TextView) rootView.findViewById(R.id.roamingBioText);
+        badQualitiesInfo = (TextView) rootView.findViewById(R.id.roamingBadQualitiesText);
+        goodQualitiesInfo = (TextView) rootView.findViewById(R.id.roamingGoodQualitiesText);
+        imageButton = (ImageButton) rootView.findViewById(R.id.roamingImageButton);
+        age = (TextView) rootView.findViewById(R.id.roamingAgeContent);
+        sexText = (TextView)rootView.findViewById(R.id.roamingSexContent);
+        location = (TextView) rootView.findViewById(R.id.roamingLocationTextValue);
+        //Toolbar toolbar = (Toolbar) rootView.findViewById(R.id.toolbar);
+    }
+
+
+    //Recreate the Listener if it had been removed due to Pause
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        //FireBase Reference for User Profile Images will go here
+        //for now static URL
+        listenUSER_PROFILE_PIC();
+        //profilePicURL = "https://s3-us-west-1.amazonaws.com/dealbreaker/classpic.jpg";
+        //downloadImages.execute(profilePicURL);
+
+        //Firebase Listener to Update UI Function calls
+        //Function call for listening for the user_info section: used right now for Name of User
+        listenUSER_INFO();
+
+        //Function call for listening to update the user qualities of the Firebase Database
+        listenUSER_QUALITIES();
+
+
+        Log.d("Event Listeners Back: ", "In User Profile Fragment!");
+    }
+
+    //Destroy the Listener if the App is paused
+    @Override
+    public void onPause() {
+        super.onPause();
+        //if statement due to onDestroy being called in a the UpdateImage after replacing due to Orientation Change
+        if (userInfoListener != null && userQualitiesListener != null && profilePicListener != null) {
+            userInfoREF.removeEventListener(userInfoListener);
+            profilePicREF.removeEventListener(profilePicListener);
+            userQualitiesREF.removeEventListener(userQualitiesListener);
+            Log.d("Event Listeners Gone: ", "In User Profile Fragment!");
+        }
+    }
+    //Destroy the Listener if the App is destroyed/exited
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        //if statement due to onDestroy being called in a the UpdateImage after replacing due to Orientation Change
+        if(userInfoListener != null && userQualitiesListener != null && profilePicListener != null) {
+            userInfoREF.removeEventListener(userInfoListener);
+            profilePicREF.removeEventListener(profilePicListener);
+            userQualitiesREF.removeEventListener(userQualitiesListener);
+            Log.d("Event Listeners Gone: ", "In User Profile Fragment!");
+        }
+    }
+
+    private void listenUSER_INFO(){
+        userInfoREF = new Firebase(Constants.FIREBASE_URL_USERS).child(userName).child(Constants.FIREBASE_LOC_USER_INFO);
+        //Add the value Event Listener so if data has already been inputted by the user then it will
+        //pre-populated with existing data
+        userInfoListener =  userInfoREF.addValueEventListener(new ValueEventListener() {
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // You can use getValue to deserialize the data at dataSnapshot
+                User user = dataSnapshot.getValue(User.class);
+                // If there was no data at the location we added the listener, then
+                if (user != null) {
+                    //displays first and last name of user to profile page
+                    String firstAndLastName = user.getFirstName() + " " + user.getLastName();
+                    //
+                    personalName.setText(firstAndLastName);
+                    //displays the age of the user
+                    age.setText(user.getAge().toString());
+                    sexText.setText(user.getSex());
+                    location.setText(user.getLocation());
+                }
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+                Log.e(getString(R.string.LogTagUserProfile),
+                        getString(R.string.FirebaseOnCancelledError) +
+                                firebaseError.getMessage());
+            }
+        });
+    }
+
+    private void listenUSER_QUALITIES(){
+        //Firebase section for Qualities
+        userQualitiesREF = new Firebase(Constants.FIREBASE_URL_USERS).child(userName).child(Constants.FIREBASE_LOC_USER_QUALITIES);
+        userQualitiesListener = userQualitiesREF.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                UserQualities userQualities = dataSnapshot.getValue(UserQualities.class);
+                if (userQualities != null) {
+                    bio_info.setText(userQualities.getBiography());
+                    badQualitiesInfo.setText(userQualities.getBadQualities());
+                    // Until accepted, Good Qualities are not shown.
+                    goodQualitiesInfo.setText("");
+                } else {
+                    //Will send a toast message that will pop up notifying user that there was an error
+                    Toast.makeText(getContext(), "Failed to Retrieve Info!!", Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+                Log.e(getString(R.string.LogTagUserProfile),
+                        getString(R.string.FirebaseOnCancelledError) +
+                                firebaseError.getMessage());
+            }
+        });
+
+    }
+
+    public void listenUSER_PROFILE_PIC(){
+        profilePicREF = new Firebase(Constants.FIREBASE_URL_IMAGES).child(userName).child(Constants.FIREBASE_LOC_PROFILE_PIC);
+        profilePicListener = profilePicREF.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                UserQualities profilePicSource = dataSnapshot.getValue(UserQualities.class);
+                if (profilePicSource != null) {
+                    downloadImages = new DownloadImages(imageButton, getActivity());
+                    profilePicURL = profilePicSource.getProfilePic();
+                    downloadImages.execute(profilePicURL);
+                    downloadImages = null;
+                } else {
+                    downloadImages = new DownloadImages(imageButton, getActivity());
+                    downloadImages.execute("dummyURL");
+                    downloadImages = null;
+                }
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+                Log.e(getString(R.string.LogTagUserProfile),
+                        getString(R.string.FirebaseOnCancelledError) +
+                                firebaseError.getMessage());
+            }
+        });
+    }
+
+
+}
