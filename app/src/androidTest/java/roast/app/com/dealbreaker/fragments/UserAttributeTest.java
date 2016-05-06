5fb11682835a84f91fd5206cdc79461dde39a11e
willgoishi <william3296@mail.fresnostate.edu>
2016-05-05 17:18:06 -0700

    addition to testing

--- app/src/androidTest/java/roast/app/com/dealbreaker/fragments/UserAttributeTest.java
+++ app/src/androidTest/java/roast/app/com/dealbreaker/fragments/UserAttributeTest.java
@@ -5,7 +5,6 @@ import junit.framework.TestCase;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import roast.app.com.dealbreaker.models.Age;
@@ -23,10 +22,30 @@ public class UserAttributeTest extends TestCase {
     public void testIsThisDateValid(String dataToValidate, String dateFormat) throws Exception {
 
         //The date is not NULL
-        assertNotNull(dataToValidate);
-        assertEquals(dataToValidate.length(), 10);   //"MM/DD/YYYY" length equals 10 characters
-        SimpleDateFormat SDF = new SimpleDateFormat(dateFormat);
+        //assertNotNull(dataToValidate);
+        //assertEquals(dataToValidate.length(), 10);   //"MM/DD/YYYY" length equals 10 characters
+
+        //test data null string
+        dataToValidate = "";
+        assertEquals(false, UA.isThisDateValid(dataToValidate, dateFormat));
+
+        //test data on user birthday that is between 18 and 130
+        dataToValidate = "9/12/1990";
+        dateFormat = "";
+        assertEquals(true, UA.isThisDateValid(dataToValidate, dateFormat));
+
+        //test data on user birthday younger than 18
+        dataToValidate = "9/12/2000";
+        dateFormat = "";
+        assertEquals(true, UA.isThisDateValid(dataToValidate, dateFormat));
+
+        //test age beyond 130
+        dataToValidate = "9/12/1885";
+        dateFormat = "";
+        assertEquals(true, UA.isThisDateValid(dataToValidate, dateFormat));
         UA.isThisDateValid(dataToValidate, dateFormat);
+
+
     }
 
     @Test
@@ -40,7 +59,8 @@ public class UserAttributeTest extends TestCase {
 
         Integer dateTest1Age = Integer.valueOf(a.calculateAge(birth));
         Integer futureAge = Integer.valueOf(a.calculateAge(birthFutureCase));
-        //18-130 range
+
+        //18-130 range not including 130
         //String dateTest2Age = "";
         assertSame(20, dateTest1Age);
 
